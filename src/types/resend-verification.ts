d23a825c9b6f5d691ece65b39813d432ef93994b
Christian Llontop <chrisllontop@icloud.com>
2024-05-13 23:08:31 -0500

    Move types to a single file

--- src/types/resend-verification.ts
+++
@@ -1,9 +0,0 @@
-export type ResendVerificationRequest = {
-  /** Internal certificate ID, also referred to as certificate hash. */
-  id: string;
-};
-
-export type ResendVerificationResponse = {
-  /** Returns `1` to indicate that your API request was successful. */
-  success: 1 | 0;
-};
